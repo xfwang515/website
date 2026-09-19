8426e2c5961782641b801ce86721c799622575a7
Felix Wang <xfwang515@users.noreply.github.com>
2026-09-19 17:15:46 +0800

    改邮箱

--- data-deletion/data-deletion.js
+++ data-deletion/data-deletion.js
@@ -136,7 +136,7 @@
       "Thank you."
     ].join("\n");
 
-    window.location.href = "mailto:privacy@coolgc.com?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
+    window.location.href = "mailto:contact@coolgc.com?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
   }
 
   // Play Console 可以使用 ?app=gemsort，让审核人员直接看到匹配的游戏名称和包名。
